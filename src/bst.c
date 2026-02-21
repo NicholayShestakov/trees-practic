d1b4cc7d57eddbcb6e28148db8fd895383587596
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-02-21 15:13:10 +0300

    Add realization of size, min, max
    Co-authored-by: MariiaByvalkina

--- src/bst.c
+++ src/bst.c
@@ -90,3 +90,69 @@ void bstFree(BST* tree)
     free(tree->root);
     free(tree);
 }
+
+// int max(int a, int b)
+// {
+//     if (a > b) {
+//         return a;
+//     }
+//     return b;
+// }
+
+// int bstHeightRecursion(Node* node, int height)
+// {
+//     if ()
+// }
+
+// int bstHeight(BST* tree)
+// {
+//     if (tree->root == NULL) {
+//         return 0;
+//     }
+
+//     return max(bstHeightRecursion(tree->root->leftChild, 1), bstHeightRecursion(tree->root->rightChild, 1));
+// }
+
+int bstSizeRecursion(Node* node)
+{
+    if (node == NULL) {
+        return 0;
+    }
+
+    return bstSizeRecursion(node->leftChild) + bstSizeRecursion(node->rightChild) + 1;
+}
+
+int bstSize(BST* tree)
+{
+    if (tree->root == NULL) {
+        return 0;
+    }
+
+    return bstSizeRecursion(tree->root->leftChild) + bstSizeRecursion(tree->root->rightChild) + 1;
+}
+
+int bstMin(BST* tree)
+{
+    if (tree->root == NULL) {
+        return -1;
+    }
+
+    Node* currentNode = tree->root;
+    while (currentNode->leftChild != NULL) {
+        currentNode = currentNode->leftChild;
+    }
+    return currentNode->value;
+}
+
+int bstMax(BST* tree)
+{
+    if (tree->root == NULL) {
+        return -1;
+    }
+
+    Node* currentNode = tree->root;
+    while (currentNode->rightChild != NULL) {
+        currentNode = currentNode->rightChild;
+    }
+    return currentNode->value;
+}
