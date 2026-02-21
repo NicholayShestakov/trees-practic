d4360f627a22bc6bde1a7e30d3f2fe33290126b8
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-02-21 14:56:47 +0300

    Add realization file
    Co-authored-by: MariiaByvalkina

---
+++ src/bst.c
@@ -0,0 +1,92 @@
+#include "bst.h"
+
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node* leftChild;
+    struct Node* rightChild;
+} Node;
+
+typedef struct BST {
+    Node* root;
+} BST;
+
+BST* bstCreate()
+{
+    BST* tree = calloc(1, sizeof(*tree));
+    return tree;
+}
+
+void bstInsert(BST* tree, int value)
+{
+    Node* newNode = calloc(1, sizeof(*newNode));
+    newNode->value = value;
+
+    Node* currentNode = tree->root;
+    while (currentNode != NULL) {
+        if (currentNode->value == value) {
+            free(newNode);
+            return;
+        }
+        if (currentNode->value > value) {
+            if (currentNode->leftChild == NULL) {
+                currentNode->leftChild = newNode;
+                return;
+            }
+            currentNode = currentNode->leftChild;
+        }
+        if (currentNode->value < value) {
+            if (currentNode->rightChild == NULL) {
+                currentNode->rightChild = newNode;
+                return;
+            }
+            currentNode = currentNode->rightChild;
+        }
+    }
+    tree->root = newNode;
+}
+
+bool bstContains(BST* tree, int value)
+{
+    Node* currentNode = tree->root;
+    while (currentNode != NULL) {
+        if (currentNode->value == value) {
+            return true;
+        }
+        if (currentNode->value > value) {
+            if (currentNode->leftChild == NULL) {
+                return false;
+            }
+            currentNode = currentNode->leftChild;
+        }
+        if (currentNode->value < value) {
+            if (currentNode->rightChild == NULL) {
+                return false;
+            }
+            currentNode = currentNode->rightChild;
+        }
+    }
+    return false;
+}
+
+void bstFreeRecursion(Node* node)
+{
+    if (node == NULL) {
+        return;
+    }
+    bstFreeRecursion(node->leftChild);
+    bstFreeRecursion(node->rightChild);
+    free(node);
+}
+
+void bstFree(BST* tree)
+{
+    if (tree->root == NULL) {
+        return;
+    }
+    bstFreeRecursion(tree->root->leftChild);
+    bstFreeRecursion(tree->root->rightChild);
+    free(tree->root);
+    free(tree);
+}
