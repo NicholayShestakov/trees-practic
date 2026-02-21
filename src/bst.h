29b93c864d0144bfaaa2f7166b931f84f8e16500
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-02-21 14:55:29 +0300

    Add header file
    Co-authored-by: MariiaByvalkina

---
+++ src/bst.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef struct BST BST;
+
+// Создаёт двоичное дерево поиска без узлов и возвращает указатель на него.
+BST* bstCreate();
+
+// Вставка значения в дерево.
+void bstInsert(BST* tree, int value);
+
+// Проверка на наличие значения в дереве.
+// Возвращает true, если значение есть, и false, если нет.
+bool bstContains(BST* tree, int value);
+
+// Освобождение памяти, занимаемой деревом.
+void bstFree(BST* tree);
